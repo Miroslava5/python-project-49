abf397eb4cd578982a30521806eca2f73e0af5c1
Miroslava Piatibratova <miroslava0825@mail.ru>
2023-04-13 21:12:03 +0300

    Добавление flake8 и CodeCLimat.com

--- Brain_games/cli.py
+++ Brain_games/cli.py
@@ -1,9 +1,11 @@
 import prompt
 
+
 def welcome_user():
-        name = prompt.string('May I have your name? ')
-        print(f'Hello, {name}!')
-        return
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
+    return
+
 
 if __name__ == '__main__':
     welcome_user()
--- Makefile
+++ Makefile
@@ -12,3 +12,6 @@ publish:
 
 package-install:
 	python3 -m pip install --user dist/hexlet_code-0.1.0-py3-none-any.whl
+	
+make lint:
+	poetry run flake8 Brain_games
