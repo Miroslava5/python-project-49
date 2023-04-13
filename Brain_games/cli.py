2c1b8cb9d3060f299f9e29cd5bf7eb26d0ddaacd
Miroslava Piatibratova <miroslava0825@mail.ru>
2023-04-13 20:29:40 +0300

    Первое приветсвие и настройка

---
+++ Brain_games/cli.py
@@ -0,0 +1,9 @@
+import prompt
+
+def welcome_user():
+        name = prompt.string('May I have your name? ')
+        print(f'Hello, {name}!')
+        return
+
+if __name__ == '__main__':
+    welcome_user()
---
+++ Brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from Brain_games.cli import welcome_user
+
+def main():
+	print ('Welcome to the Brain Games!')
+	welcome_user()
+
+if __name__ == '__main__':
+	main()
