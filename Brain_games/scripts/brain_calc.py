bc872a5ef0934cb837cf32783ad62a2d1bcf4bdc
Miroslava Piatibratova <miroslava0825@mail.ru>
2023-04-18 08:44:25 +0300

    обновлены связи, проведена настройка и отладка игры brain_calс

---
+++ Brain_games/scripts/brain_calc.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from Brain_games.Games.calc import do_game_calc
+
+
+def main():
+    print('Welcome to the Brain Games!')
+    do_game_calc()
+
+
+if __name__ == '__main__':
+    main()
--- Makefile
+++ Makefile
@@ -6,6 +6,9 @@ brain-games:
 
 brain-even:
 	poetry run brain-even
+	
+brain-calc:
+	poetry run brain-calc
 
 build:
 	poetry build
